24070a22830516498d3a89c35cf8739ed7947fdd
Ike Patton <ike.patton16@gmail.com>
2024-09-16 09:34:02 -0400

    move to app

--- backup/sleeperSpider.py
+++ backup/sleeperSpider.py
@@ -6,12 +6,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    print("test")
     return render_template("index.html")
 
 @app.route("/favorites", methods=["POST"])
 def favorites():
     username = request.form.get("username")
-    if not username or not username.isalnum():
+    print("TEST")
+    print(username)
+    print(username.isalnum())
+    #if not username: #or ["'", '"', ' ', '.'] in username:
+    if not username.isalnum() or not username:
         return render_template("failure.html", username=username)
 
     favorites = do.getLeagueMatesFavorites(do.getUserID(username))
